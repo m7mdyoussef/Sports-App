39f2ac29de3938156bbdd92d3d82462d7258ebaa
Wesam <wesam@Wesams-Mac.local>
2021-04-20 01:27:00 +0200

    build teams details page design

---
+++ Sports App/Controllers/TeamsDetailsViewController/TeamsDetailsTableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TeamsDetailsTableViewController.swift
+//  Sports App
+//
+//  Created by Wesam on 4/19/21.
+//  Copyright © 2021 mohamed youssef. All rights reserved.
+//
+
+import UIKit
+
+class TeamsDetailsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
